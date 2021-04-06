538b9e44e278fa2c23d582f97f8d72d1f1860820
falangames <77401556+falangames@users.noreply.github.com>
2021-04-06 14:58:29 +0300

    begin2

---
+++ originemV3/Assets/Scripts/StarControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarControl : MonoBehaviour
+{
+    GameObject[] target;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+    int i = 0;
+    void FixedUpdate()
+    {
+        if (i != 500)
+        {
+            i += 1;
+            target = GameObject.FindGameObjectsWithTag("StarsBreakPoint");
+            transform.position = Vector3.Lerp(transform.position, target[0].transform.position, Time.deltaTime * 8f);
+        }
+    }
+}
---
+++ originemV3/Assets/Scripts/StarSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSpawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+
+    private void FixedUpdate()
+    {
+        objectPooler.SpawnFromPool("Star", new Vector3(Random.Range(transform.position.x - 600, transform.position.x + 600), Random.Range(transform.position.y - 600, transform.position.y + 600), Random.Range(transform.position.z - 600, transform.position.z + 600)), Quaternion.identity);
+    }
+}
